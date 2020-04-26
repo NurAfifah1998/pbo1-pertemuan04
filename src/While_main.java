6aecf00473a32dbe8a00b92d852d3a11892d834c
nurafifah <naafifah98@gmail.com>
2020-04-26 23:27:55 +0800

    menambahkan isian While hasil dari inputan isiGelas hingga sampi output penuh

---
+++ src/While_main.java
@@ -0,0 +1,21 @@
+public class While_main {
+
+        public static void main(String[] args) {
+            int isiGelas = 0;
+            int penuh =225;
+
+            System.out.println("Isi Gelas Sekearang : "+ isiGelas + "ml");
+            System.out.println("Isi gelas Ketika Penuh :" + penuh +"ml");
+
+            while (isiGelas != penuh){
+                isiGelas++;
+                System.out.println("Sedang mengisi Gelas...");
+                System.out.println("Isi Gelas sekarang : " + isiGelas + "ml");
+
+            }
+            System.out.println("Finale: Isi Gelas sekarang :"+ isiGelas + "ml");
+
+
+        }
+
+}
