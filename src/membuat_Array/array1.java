cf9d421a28bf6e7d1fe224d8bc6ab90c0a8e1c85
nurafifah <naafifah98@gmail.com>
2020-04-27 00:28:35 +0800

    perulanagan array sederhana ,menampilkan hasil leght dan inputan hasil varibel yg terpilih

--- src/membuat_Array/array1.java
+++ src/membuat_Array/array1.java
@@ -1,4 +1,6 @@
-public class Array {
+package membuat_Array;
+
+public class array1 {
     public static void main(String[] args) {
         int nilaiArr[] = {20,25,30,35,40,45,50,55,60,65,70};
         System.out.println(nilaiArr.length);
