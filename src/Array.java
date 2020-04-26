01c39686f825ecff5c49c8f1a8bc644c6d3a493f
nurafifah <naafifah98@gmail.com>
2020-04-27 00:00:49 +0800

    array kumpulan variable yang sama , contoh array sederhana dengan output leght dan pemilihan  nilai

---
+++ src/Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[] args) {
+        int nilaiArr[] = {20,25,30,35,40,45,50,55,60,65,70};
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[8]);
+
+    }
+}
