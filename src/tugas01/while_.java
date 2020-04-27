4f5981f3db32a221d771bc52a63b8ce475d0eb14
nurafifah <naafifah98@gmail.com>
2020-04-27 10:14:57 +0800

    perulangan mengunakan while (jumlah barang)

---
+++ src/tugas01/while_.java
@@ -0,0 +1,22 @@
+package tugas01;
+
+public class while_ {
+    public static void main(String[] args) {
+        int isianBarang = 0;
+        int lusin_1 = 12;
+        int lusin_2 = 24;
+
+        System.out.println("masukkan jumlah barang :"+isianBarang+"buah");
+        System.out.println("hasil dalam 1 lusin :"+lusin_1+"buah");
+        System.out.println("hasil dalam 2 lusin :"+lusin_2+"buah");
+
+        while (isianBarang != lusin_1){
+            isianBarang++;
+            System.out.println("memasukkan jumlah barang...");
+            System.out.println("Isi barang : " + isianBarang + "buah");
+
+        }
+        System.out.println("Akhir Isi baramng:"+ isianBarang + "buah");
+
+    }
+}
