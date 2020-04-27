e07aa2d08965e1e73a9247c74dd5cecb1aeff366
nurafifah <naafifah98@gmail.com>
2020-04-27 10:31:36 +0800

    penggunana do_while dalam perulangan isi barang 1 lusin

---
+++ src/tugas01/do_while_.java
@@ -0,0 +1,21 @@
+package tugas01;
+
+public class do_while_ {
+    public static void main(String[] args) {
+        int isianBarang = 0;
+        int lusin_1 = 12;
+        int lusin_2 = 24;
+
+        System.out.println("masukkan jumlah barang :"+isianBarang+"buah");
+        System.out.println("hasil dalam 1 lusin :"+lusin_1+"buah");
+        System.out.println("hasil dalam 2 lusin :"+lusin_2+"buah");
+
+        do {
+            isianBarang++;
+            System.out.println("memasukkan jumlah barang...");
+            System.out.println("Isi barang : " + isianBarang+ "buah");
+
+        } while (isianBarang != lusin_1);
+        System.out.println("Finale: Isibarang:"+ isianBarang + "buah dalam 1 lusin");
+    }
+}
