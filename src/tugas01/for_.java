8c8d0886e3473ce406053750fc87ee742f819dde
nurafifah <naafifah98@gmail.com>
2020-04-27 10:38:37 +0800

    penggnaan for dalam perlangan pengisian barang dalam 1 lusin

---
+++ src/tugas01/for_.java
@@ -0,0 +1,23 @@
+package tugas01;
+
+public class for_ {
+    public static void main(String[] args) {
+        int isianBarang = 0;
+        int lusin_1 = 12;
+        int lusin_2 = 24;
+
+        System.out.println("masukkan jumlah barang :"+isianBarang+"buah");
+        System.out.println("hasil dalam 1 lusin :"+lusin_1+"buah");
+        System.out.println("hasil dalam 2 lusin :"+lusin_2+"buah");
+
+        for (isianBarang=0;isianBarang<=lusin_1;isianBarang++){
+            isianBarang++;
+            System.out.println("memasukkan jumlah barang...");
+            System.out.println("Isi Barang : " + isianBarang + "Buah");
+
+        }
+        System.out.println("Finale: Isi Gelas sekarang :"+ isianBarang + "Buah dalam 1 lusin");
+
+
+    }
+}
